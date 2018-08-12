fc886e32f0ed9c390e8e8786441b4559e0840ac1
Ivan Gagis <igagis@gmail.com>
2018-08-12 19:09:35 +0300

    android fix: do not use dlopen()

---
+++ android/fakecreateapp/src/main/cpp/createapp.cpp
@@ -0,0 +1,6 @@
+#include "../../../../../src/mordavokne/AppFactory.hpp"
+
+
+std::unique_ptr<mordavokne::App> mordavokne::createApp(int argc, const char** argv, const utki::Buf<std::uint8_t> savedState){
+	return nullptr;
+}
