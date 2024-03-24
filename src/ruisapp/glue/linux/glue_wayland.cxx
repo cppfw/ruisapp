d24d9f6e3fbd32e2bdd2b185d2bc5ae0d4a818c6
Ivan Gagis <igagis@gmail.com>
2024-03-24 19:11:53 +0200

    wayland build

--- src/ruisapp/glue/linux/glue_wayland.cxx
+++ src/ruisapp/glue/linux/glue_wayland.cxx
@@ -19,13 +19,32 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* ================ LICENSE END ================ */
 
-#include <utki/destructable.hpp>
 #include <wayland-client-core.h>
 #include <wayland-client-protocol.h>
+
+#include <utki/destructable.hpp>
+#include <papki/fs_file.hpp>
+
+#ifdef RUISAPP_RENDER_OPENGL
+#	include <GL/glew.h>
+// #	include <GL/glx.h>
+#	include <ruis/render/opengl/renderer.hpp>
+
+#elif defined(RUISAPP_RENDER_OPENGLES)
+#	include <EGL/egl.h>
+#	include <GLES2/gl2.h>
+#	include <ruis/render/opengles/renderer.hpp>
+
+#else
+#	error "Unknown graphics API"
+#endif
 
 #include "../../../out/xdg-shell-client-protocol.h"
 
 #include "../../application.hpp"
+
+#include "../friend_accessors.cxx" // NOLINT(bugprone-suspicious-include)
+#include "../unix_common.cxx" // NOLINT(bugprone-suspicious-include)
 
 using namespace std::string_view_literals;
 
@@ -37,14 +56,14 @@ struct window_wrapper : public utki::destructable {
 		wl_display* disp;
 
 		wl_compositor* compositor = nullptr;
-		xdg_wm_base* xdg_wm_base = nullptr;
+		xdg_wm_base* wm_base = nullptr;
 
-		static void xdg_wm_base_ping(void* data, xdg_wm_base* xdg_wm_base, uint32_t serial)
+		static void xdg_wm_base_ping(void* data, xdg_wm_base* wm_base, uint32_t serial)
 		{
-			xdg_wm_base_pong(xdg_wm_base, serial);
+			xdg_wm_base_pong(wm_base, serial);
 		}
 
-		static const xdg_wm_base_listener xdg_wm_base_listener = {
+		constexpr static const xdg_wm_base_listener wm_base_listener = {
 			.ping = &xdg_wm_base_ping //
 		};
 
@@ -59,14 +78,16 @@ struct window_wrapper : public utki::destructable {
 			ASSERT(data)
 			auto& self = *static_cast<display_wrapper*>(data);
 
-			LOG([](auto& o) {
+			LOG([&](auto& o) {
 				o << "got a registry event for: " << interface << ", id = " << id << std::endl;
 			});
 			if (std::string_view(interface) == "wl_compositor"sv) {
-				self.compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+                void* compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+				self.compositor = static_cast<wl_compositor*>(compositor);
 			} else if (std::string_view(interface) == xdg_wm_base_interface.name) {
-				self.xdg_wm_base = wl_registry_bind(registry, id, &xdg_wm_base_interface, 1);
-				xdg_wm_base_add_listener(self.xdg_wm_base, &xdg_wm_base_listener, NULL);
+                void* wm_base = wl_registry_bind(registry, id, &xdg_wm_base_interface, 1);
+				self.wm_base = static_cast<xdg_wm_base*>(wm_base);
+				xdg_wm_base_add_listener(self.wm_base, &wm_base_listener, nullptr);
 			}
 		}
 
@@ -76,12 +97,12 @@ struct window_wrapper : public utki::destructable {
 			uint32_t id
 		)
 		{
-			LOG([](auto& o) {
+			LOG([&](auto& o) {
 				o << "got a registry losing event, id = " << id << std::endl;
 			});
 		}
 
-		static const wl_registry_listener listener = {
+		constexpr static const wl_registry_listener listener = {
 			&global_registry_handler, //
 			&global_registry_remover
 		};
@@ -109,10 +130,129 @@ struct window_wrapper : public utki::destructable {
 	} display;
 
 	window_wrapper(const window_params& wp) {}
+
+    	ruis::real get_dots_per_inch()
+	{
+        // TODO:
+        return 96;
+
+		// int src_num = 0;
+
+		// constexpr auto mm_per_cm = 10;
+
+		// ruis::real value =
+		// 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	((ruis::real(DisplayWidth(this->display.display, src_num))
+		// 	  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	  / (ruis::real(DisplayWidthMM(this->display.display, src_num)) / ruis::real(mm_per_cm)))
+		// 	 // NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	 +
+		// 	 // NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	 (ruis::real(DisplayHeight(this->display.display, src_num))
+		// 	  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	  / (ruis::real(DisplayHeightMM(this->display.display, src_num)) / ruis::real(mm_per_cm)))) /
+		// 	2;
+		// constexpr auto cm_per_inch = 2.54;
+		// value *= ruis::real(cm_per_inch);
+		// return value;
+	}
+
+	ruis::real get_dots_per_pp()
+	{
+        // TODO:
+        return 1;
+
+		// // TODO: use scale factor only for desktop monitors
+		// if (this->scale_factor != ruis::real(1)) {
+		// 	return this->scale_factor;
+		// }
+
+		// int src_num = 0;
+		// r4::vector2<unsigned> resolution(
+		// 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	DisplayWidth(this->display.display, src_num),
+		// 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	DisplayHeight(this->display.display, src_num)
+		// );
+		// r4::vector2<unsigned> screen_size_mm(
+		// 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	DisplayWidthMM(this->display.display, src_num),
+		// 	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		// 	DisplayHeightMM(this->display.display, src_num)
+		// );
+
+		// return application::get_pixels_per_pp(resolution, screen_size_mm);
+	}
 };
+
+window_wrapper& get_impl(const std::unique_ptr<utki::destructable>& pimpl)
+{
+	ASSERT(dynamic_cast<window_wrapper*>(pimpl.get()))
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-static-cast-downcast)
+	return static_cast<window_wrapper&>(*pimpl);
+}
+
+// TODO:
+// window_wrapper& get_impl(application& app)
+// {
+// 	return get_impl(get_window_pimpl(app));
+// }
+
 } // namespace
+
+application::application(std::string name, const window_params& wp) :
+name(std::move(name)),
+	window_pimpl(std::make_unique<window_wrapper>(wp)),
+	gui(utki::make_shared<ruis::context>(
+#ifdef RUISAPP_RENDER_OPENGL
+		utki::make_shared<ruis::render_opengl::renderer>(),
+#elif defined(RUISAPP_RENDER_OPENGLES)
+		utki::make_shared<ruis::render_opengles::renderer>(),
+#else
+#	error "Unknown graphics API"
+#endif
+		utki::make_shared<ruis::updater>(),
+		[this](std::function<void()> a) {
+            // TODO:
+			// get_impl(get_window_pimpl(*this)).ui_queue.push_back(std::move(a));
+		},
+		[this](ruis::mouse_cursor c) {
+            // TODO:
+			// auto& ww = get_impl(*this);
+			// ww.set_cursor(c);
+		},
+		get_impl(window_pimpl).get_dots_per_inch(),
+		get_impl(window_pimpl).get_dots_per_pp()
+	)),
+	storage_dir(initialize_storage_dir(this->name))
+{
+    // TODO:
+}
+
+void application::swap_frame_buffers(){
+    // TODO:
+}
+
+void application::set_mouse_cursor_visible(bool visible){
+    // TODO:
+}
+
+void application::set_fullscreen(bool fullscreen){
+    // TODO:
+}
+
+void ruisapp::application::quit()noexcept{
+    // TODO:
+}
 
 int main(int argc, const char** argv)
 {
+    std::unique_ptr<ruisapp::application> app = create_app_unix(argc, argv);
+	if (!app) {
+		return 1;
+	}
+
+    // TODO:
+
 	return 0;
 }
