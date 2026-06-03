37e8769f17ff93e57fd6716db52cb448eb453a9c
Ivan Gagis <igagis@gmail.com>
2026-06-03 15:21:56 +0300

    format

--- src/ruisapp/glue/macos/application.hxx
+++ src/ruisapp/glue/macos/application.hxx
@@ -26,10 +26,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include <utki/destructable.hpp>
 
-#include "cocoa.hxx"
-
 #include "../../application.hpp"
 
+#include "cocoa.hxx"
 #include "window.hxx"
 
 namespace {
