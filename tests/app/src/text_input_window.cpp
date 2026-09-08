c4372df3de3a66dbc76a77787497b3ae0ba7f811
Ivan Gagis <igagis@gmail.com>
2026-09-09 02:27:33 +0300

    update

--- tests/app/src/text_input_window.cpp
+++ tests/app/src/text_input_window.cpp
@@ -6,7 +6,7 @@
 #include <ruis/widget/group/collapse_area.hpp>
 #include <ruis/widget/button/impl/image_push_button.hpp>
 #include <ruis/widget/label/gap.hpp>
-#include <ruis/widget/input/text_field.hpp>
+#include <ruis/widget/input/bare_text_field.hpp>
 
 using namespace std::string_literals;
 using namespace std::string_view_literals;
@@ -121,7 +121,7 @@ utki::shared_ref<ruis::window> make_text_input_window(
                     }
                 },
                 {
-                    m::text_field(c,
+                    m::bare_text_field(c,
                         {
                             .layout_params{
                                 .dims{ruis::dim::fill, ruis::dim::max}
